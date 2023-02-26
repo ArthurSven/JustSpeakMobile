4498765754eed0c2545e777485e42f9378a70567
ArthurSven <arthursvenmsiska@gmail.com>
2023-02-26 22:51:07 +0200

    German sentence page has been started, still needs to be updated with content

---
+++ app/src/main/java/com/lauterbach/justspeak/German/GermanGrammar/GermanSentences.kt
@@ -0,0 +1,13 @@
+package com.lauterbach.justspeak.German.GermanGrammar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lauterbach.justspeak.R
+
+class GermanSentences : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentences_german)
+
+    }
+}
--- app/src/main/java/com/lauterbach/justspeak/German/GermanGrammarFragment.kt
+++ app/src/main/java/com/lauterbach/justspeak/German/GermanGrammarFragment.kt
@@ -1,11 +1,13 @@
 package com.lauterbach.justspeak.German
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.card.MaterialCardView
 import com.lauterbach.justspeak.German.GermanGrammar.*
 import com.lauterbach.justspeak.R
 import com.lauterbach.justspeak.databinding.FragmentGermanGrammarBinding
@@ -46,8 +48,18 @@ open class GermanGrammarFragment : Fragment() {
             val intent = Intent(this@GermanGrammarFragment.requireContext(), GermanPronouns::class.java)
             startActivity(intent)
         }
+        binding.GermanSentenceCard.setOnClickListener {
+            val intent = Intent(this@GermanGrammarFragment.requireContext(), GermanSentences::class.java)
+            startActivity(intent)
+        }
         return binding.root
 
     }
+    /*fun buttonClick(karte: MaterialCardView, activ: Activity) {
+        karte.setOnClickListener {
+            val intent = Intent(this@GermanGrammarFragment.requireContext(),activ::class.java)
+            startActivity(intent)
+        }
+    } */
 
 }
